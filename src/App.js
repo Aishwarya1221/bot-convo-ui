3765f1fb40847d59a4df5b33e507e5eb693fdfea
Aishwarya A <124003014@sastra.ac.in>
2025-03-22 22:25:32 +0530

    basic ui set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './components/chatbot'; // ✅ lowercase file, uppercase component!
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Website</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
